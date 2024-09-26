344f68c5c20c96a7c8b15abaa7da6a2de280e6af
Sean O'Connor <sfoconnor@Seans-MacBook-Pro-2.local>
2024-09-25 19:47:07 -0500

    Added some folder structure and contents for day 1

---
+++ Beginner/Day_1/day1.py
@@ -0,0 +1,5 @@
+#For Day 1, we have a band name generator utilizing print and input calls
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 100DaysOfPython
+- Day 1 Notes:
+    - Refresher: len() for length
+    - input() for user input
+- Day 2 Notes:
+    - Temp
